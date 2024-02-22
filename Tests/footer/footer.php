8666709052d7e3784e9e163f427c842046718815
dvvuuh <dauh13980@gmail.com>
2024-02-22 14:13:00 +0100

    create footer and responsive footer, couleur footer a changé

---
+++ Tests/footer/footer.php
@@ -0,0 +1,41 @@
+
+<html>
+    <link rel="stylesheet" href="footerStyle.css">
+    <link rel="stylesheet" href="./../../Site_definitif/css/normalize.css">
+    <footer>
+        <section class="mainFooter">
+            <div class="copyright">
+                <ul>
+                    <li><a href="#">Mentions légales</a></li>
+                    <li><a href="#">icones: icones8</a></li>
+                </ul>
+            </div>
+
+            <div class="second">
+                <ul>
+                    <li><a href="#">Inscription</a></li>
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">icones: icones8</a></li>
+                </ul>
+            </div>
+
+            <div class="contact">
+                <ul>
+                    <li><a href="#">Stéphanie Bosc Marchetti</a></li>
+                    <li><a href="#">Téléphone</a></li>
+                    <li><a href="#">Email</a></li>
+                    <li><a href="#">Localité</a></li>
+                </ul>
+            </div>
+
+            <div class="social">
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/sbmactiveformation/"><img src="../../Site_definitif/Assets/icones/linknIcon.png" alt="Linkdn icon"></a></li>
+                </ul>
+                <ul>
+                    <li><a href="#"></a><img src="../../Site_definitif/Assets/icones/facebookIcon.png" alt="Linkdn icon"></li>
+                </ul>
+            </div>
+        </section>
+    </footer>
+</html>
