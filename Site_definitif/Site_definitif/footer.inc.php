a768c36d33094666f5fb210a590c0b7bb4e3cb8a
Jeremy <jmaubon@gmail.com>
2024-02-22 16:34:35 +0100

    modif accueil en accueil.php pour inclusion des fichiers .inc (include)*

---
+++ Site_definitif/Site_definitif/footer.inc.php
@@ -0,0 +1,37 @@
+<footer>
+        <section class="mainFooter">
+            <div class="copyright">
+                <ul>
+                    <li><a href="#">Mentions légales</a></li>
+                    <li><a href="#">icones: icones8</a></li>
+                </ul>
+            </div>
+
+            <div class="second">
+                <ul>
+                    <li><a href="#">Inscription</a></li>
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">icones: icones8</a></li>
+                </ul>
+            </div>
+
+            <div class="contact">
+                <ul>
+                    <li><a href="#">Stéphanie Bosc Marchetti</a></li>
+                    <li><a href="#">Téléphone</a></li>
+                    <li><a href="#">Email</a></li>
+                    <li><a href="#">Localité</a></li>
+                </ul>
+            </div>
+
+            <div class="social">
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/sbmactiveformation/"><img src="./Assets/icones/icons8-linkedin-96.png" alt="Linkdn icon"></a></li>
+                </ul>
+                <ul>
+                    <li><a href="#"></a><img src="./Assets/icones/icons8-facebook-96.png" alt="Facebook Icon"></li>
+                </ul>
+            </div>
+        </section>
+    </footer>
+</html>
