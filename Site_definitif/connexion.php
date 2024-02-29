f19ec152d9c04f45d45f4d45860f58c44b368a38
dvvuuh <dauh13980@gmail.com>
2024-02-29 15:14:00 +0100

    adding connexion and subscription pages with basic style

---
+++ Site_definitif/connexion.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--  normalize importé de : https://github.com/necolas/normalize.css/blob/master/normalize.css -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="./css/connexionStyle.css">
+
+    <title>Connexion</title>
+</head>
+<body>
+    <header>
+        <h1>JE SUIS LE HEADER</h1>
+    </header>
+    <main>
+    <section class="mainWindowCo">
+            <div class="mainTitle">
+                <h2>Formulaire de connexion</h2>
+            </div>
+
+            <div class="mainContent">
+                <form action="#" method="post">
+                
+                    <label for="nom">Nom:</label>
+                    <input type="text" id="nom" name="nom" required>
+
+                    <label for="prenom">Prénom:</label>
+                    <input type="text" id="prenom" name="prenom" required>
+
+                    <label for="date_naissance">Date de Naissance:</label>
+                    <input type="date" id="date_naissance" name="date_naissance" required>
+                    
+                    <p>Pas de compte ?<a href="./inscription.php"> <u>Cliquez ici</u></a></p>
+
+                    <input type="submit" value="Envoyer">
+                </form>
+            </div>
+        </section>
+
+    
+    </main>
+    <footer>
+        <h1>JE SUIS LE FOOTER</h1>
+    </footer>
+</body>
+</html>
+
+
---
+++ Site_definitif/inscription.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--  normalize importé de : https://github.com/necolas/normalize.css/blob/master/normalize.css -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="./css/connexionStyle.css">
+
+    <title>Incription</title>
+</head>
+<body>
+    <header>
+        <h1>JE SUIS LE HEADER</h1>
+    </header>
+    <main>
+        <section class="mainWindow">
+            <div class="mainTitle">
+                <h2>Formulaire d'Inscription</h2>
+            </div>
+
+            <div class="mainContent">
+                <form action="#" method="post">
+                
+                    <label for="nom">Nom:</label>
+                    <input type="text" id="nom" name="nom" required>
+
+                    <label for="prenom">Prénom:</label>
+                    <input type="text" id="prenom" name="prenom" required>
+
+                    <label for="date_naissance">Date de Naissance:</label>
+                    <input type="date" id="date_naissance" name="date_naissance" required>
+                    
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="phone">phone:</label>
+                    <input type="tel" id="phone" name="phone" required>
+
+                    <p>Déja un compte ?<a href="./connexion.php"> <u>Cliquez ici</u></a></p>
+
+                    <input type="submit" value="Envoyer">
+                </form>
+            </div>
+        </section>
+
+    
+    </main>
+    <footer>
+        <h1>JE SUIS LE FOOTER</h1>
+    </footer>
+</body>
+</html>
+
+
