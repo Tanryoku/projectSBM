aeaa4c42025e9f13c7a3073a610d2249015b2faa
dvvuuh <dauh13980@gmail.com>
2024-03-14 12:28:01 +0100

    creation page messagerie

---
+++ Site_definitif/messagerie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="./css/messagerieStyle.css">
+
+    <title>Messagerie</title>
+</head>
+<body>
+    <header>
+        je suis le header
+    </header>
+    <main>
+        <div class="mainHeader">
+            <span><img src="./Assets/icones/sort"></span>            
+        </div>
+
+        <div class="mainContent">
+
+        </div>
+
+        <div class="mainFooter">
+            <span><p>Message Envoyé</p></span>
+            <span><p>Message Reçus</p></span>
+            <span><p>Brouillons</p></span>
+
+        </div>
+    </main>
+    <footer>
+        je suis le footer
+    </footer>
+</body>
+</html>
