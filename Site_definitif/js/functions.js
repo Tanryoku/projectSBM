c819cac4c707836dc062f8a2c22ad602a3a4af5a
Jeremy <jmaubon@gmail.com>
2024-03-14 09:08:24 +0100

    Ajout page tirage autonome V1 (fonction pour animation unfinished + PHP)

--- Site_definitif/js/functions.js
+++ Site_definitif/js/functions.js
@@ -21,3 +21,34 @@ document.addEventListener("click", (event) => {
  * Fonctions tirage_autonome
  ************* ******/
 
+deck.addEventListener("click", function() {
+    tirage(card1, card2, card3);
+});
+
+function tirage(firstCard, scndCard, trdCard) {
+    // Premier clic sur le deck
+    if (!firstCard.classList.contains('center') &&
+        !firstCard.classList.contains('left')
+    ) {
+        console.log("Première carte tirée");
+        firstCard.classList.add('center');
+
+    //second clic sur le deck    
+    } else if (firstCard.classList.contains('center')) {
+        firstCard.classList.replace('center', 'left');
+        scndCard.classList.add('center');
+        console.log("deuxième carte tirée");
+
+    //troisième clic
+    } else if (firstCard.classList.contains('left')) {
+        scndCard.classList.replace('center', 'right');
+        trdCard.classList.add('center');
+        console.log("troisième carte tirée")
+
+    //quatrième clic
+    } else if (trdCard.classList.contains('center')) {
+        // les trois cartes ont été tirés, reboot la série ?
+    }
+}
+
+//rajouter un bouton (envoyer) lié à une variable. var = 1. Quand une carte est tiré = 0 (bouton deck), tant que la valeur est à 0, aucune carte ne peut être tiré (bouton deck), cliquer sur le bouton envoyer remet la valeur à 1 (bouton envoyer)
--- Site_definitif/js/variables.js
+++ Site_definitif/js/variables.js
@@ -1,6 +1,9 @@
 // Variable pour main tirage_autonome
-
-let card1 = document.getElementsByClassName("card-1");
-let card2 = document.getElementsByClassName("card-2");
-let card3 = document.getElementsByClassName("card-3");
-
+//Card 1-2-3 sont les élts dont les classes seront modifiés
+let card1 = document.querySelector(".card-1");
+let card2 = document.querySelector(".card-2");
+let card3 = document.querySelector(".card-3");
+//deck est le bouton pour démarrer le tirage
+let deck = document.querySelector(".empty_card");
+//submitBtn est le bouton qui permet d'enregistrer sa réponse et de tirer une nouvelle carte.
+let submitBtn = document.querySelector("#btn_envoyer")
