48a6992991f7112e84baada98bf34e91557466d8
Jeremy <jmaubon@gmail.com>
2024-05-23 14:50:06 +0200

    Update 23/05

--- Tests/Tests API visio/Site/config.js
+++
@@ -1,8 +0,0 @@
-
-function toggleText() {
-    // Obtient l'élément du texte
-    var cardText = document.getElementById("card-text");
-  
-    // Bascule entre les classes "hidden" et "visible" pour montrer ou masquer le texte
-    cardText.classList.toggle("hidden");
-  }
